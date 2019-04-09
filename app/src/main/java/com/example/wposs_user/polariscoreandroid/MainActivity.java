a66de5f931c99d7ff1c62569f8f4b6eb141291ac
Silvia Hernández Carrero <silvia.hdez.car@gmail.com>
2019-04-09 14:15:30 -0500

    Se modificó el nav_bar ajustandolo a la platilla web

--- app/src/main/java/com/example/wposs_user/polariscoreandroid/MainActivity.java
+++ app/src/main/java/com/example/wposs_user/polariscoreandroid/MainActivity.java
@@ -20,8 +20,10 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setTitle(null);
         setSupportActionBar(toolbar);
 
 

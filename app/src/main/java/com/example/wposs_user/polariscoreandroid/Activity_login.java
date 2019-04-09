141cb9d9b1ae6c00638aa41ca0610561bb12392e
hender1001 <49322975+hender1001@users.noreply.github.com>
2019-04-09 16:32:00 -0500

    Login Final y Act. preguntas

--- app/src/main/java/com/example/wposs_user/polariscoreandroid/Activity_login.java
+++ app/src/main/java/com/example/wposs_user/polariscoreandroid/Activity_login.java
@@ -16,7 +16,7 @@ public class Activity_login extends AppCompatActivity {
 
     public void menu(View v){
 
-        Intent i = new Intent(this, Actualizar_clave.class);
+        Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
     }

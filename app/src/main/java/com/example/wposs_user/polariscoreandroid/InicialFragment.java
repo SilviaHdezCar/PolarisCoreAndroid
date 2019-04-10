2cc444cf2f261bb50882880d47a3b75d8637e4b1
Silvia Hernández Carrero <silvia.hdez.car@gmail.com>
2019-04-10 08:46:09 -0500

    Mostrar pestañas iniciales

--- app/src/main/java/com/example/wposs_user/polariscoreandroid/InicialFragment.java
+++ app/src/main/java/com/example/wposs_user/polariscoreandroid/InicialFragment.java
@@ -1,18 +1,72 @@
 package com.example.wposs_user.polariscoreandroid;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class InicialFragment extends Fragment {
+
+    private AppBarLayout appBar;
+    private TabLayout tabs;
+    private ViewPager viewPager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view =inflater.inflate(R.layout.fragment_inicial, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicial, container, false);
+        View contenedor=(View)container.getParent();
+        appBar=(AppBarLayout)container.findViewById(R.id.appbar);
+        tabs=new TabLayout(getActivity());
+        tabs.setTabTextColors(Color.parseColor("#FFFFFF"), Color.parseColor("#FFFFFF"));
+        appBar.addView(tabs);
+
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        appBar.removeView(tabs);
+    }
+
+    public class ViewPagerAdapter extends FragmentStatePagerAdapter{
+
+        public ViewPagerAdapter(FragmentManager fragmentManager){
+            super(fragmentManager);
+        }
+
+        String[]tituloTabs={"Tarminales Asociadas", "Terminales autorizads"};
+
+
+
+        @Override
+        public Fragment getItem(int i) {
+
+            switch (i){
+                case 0:
+                    return new Tab_ConsultaTerminalesAsociadas();
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 }
